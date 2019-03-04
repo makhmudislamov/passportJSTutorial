73c7e0c6304e2e6ec36ad0a55b85e2971cb9cf2a
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-03-03 20:01:08 -0800

    adding routes for auth

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const router = express.Router();
+
+// auth login 
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// auth with google
+router.get('/google', (req, res) => {
+    
+});
