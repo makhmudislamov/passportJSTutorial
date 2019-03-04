4f467d1b75c08ba43a70836a06313ef74876f17b
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-03-04 14:49:26 -0800

    moved client id and object to different file

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    google: {
+        clientID: '240786198817-r6qsasnisl473bnrvlttn0fvm4nhovvq.apps.googleusercontent.com',
+        clientSecret: '20ki6SjqG6lo4iowsgkrEB_g'  
+    }
+};
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,11 +2,12 @@
 // passport-setup.js
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth');
+const keys = require('./keys')
 
 passport.use(new googleStrategy({
     // options
-    clientID: '240786198817-r6qsasnisl473bnrvlttn0fvm4nhovvq.apps.googleusercontent.com',
-    clientSecret: '20ki6SjqG6lo4iowsgkrEB_g'
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret
 }), () => {
     // callback
 });
