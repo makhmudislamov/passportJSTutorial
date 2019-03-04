61c898cab26527e2de7b080234e0a37b29325f8c
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-03-04 14:59:43 -0800

    added redirect uri

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,10 +4,14 @@ const passport = require('passport');
 const googleStrategy = require('passport-google-oauth');
 const keys = require('./keys')
 
-passport.use(new googleStrategy({
-    // options
-    clientID: keys.google.clientID,
-    clientSecret: keys.google.clientSecret
-}), () => {
-    // callback
-});
+passport.use(
+    new googleStrategy({
+        // options
+        callbackURI: '/auth/google/redirect',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+    }, () => {
+        // callback
+    })
+
+);
